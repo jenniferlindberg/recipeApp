7142bbf4b2588ffa668e5138156d30b278b27ffb
jenniferlindberg <jinjin.lindberg@gmail.com>
2022-06-01 17:08:52 +0200

    feat: updated routing

--- recipe-app/src/pages/Pages.jsx
+++ recipe-app/src/pages/Pages.jsx
@@ -1,10 +1,13 @@
 import Home from "./Home";
+import Cuisine from "./Cuisine";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 const Pages = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cuisine/:type" element={<Cuisine />} />
+    </Routes>
   );
 };
 
