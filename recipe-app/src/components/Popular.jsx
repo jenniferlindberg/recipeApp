e1ec64327be9201d0955f8f350497fb41a18b900
jenniferlindberg <jinjin.lindberg@gmail.com>
2022-06-01 15:32:21 +0200

    feat: add data to localStorage and update `Veggie.jsx`  # Please enter the commit message for your changes. Lines starting

--- recipe-app/src/components/Popular.jsx
+++ recipe-app/src/components/Popular.jsx
@@ -11,11 +11,19 @@ const Popular = () => {
   }, []);
 
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    );
-    const data = await api.json();
-    setPopular(data.recipes);
+    const check = localStorage.getItem("popular");
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+      console.log("new data:", data.recipes);
+    }
   };
 
   return (
@@ -32,7 +40,7 @@ const Popular = () => {
       >
         {popular.map((recipe) => {
           return (
-            <SplideSlide>
+            <SplideSlide key={recipe.id}>
               <Card>
                 <p>{recipe.title}</p>
                 <img src={recipe.image} alt={recipe.title} />
